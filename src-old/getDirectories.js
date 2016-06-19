2f12836077cb1b2cb56f7b90351b888d8fb7f5f8
Justin <calleja.justin@gmail.com>
2016-06-19 15:48:58 +0200

    Removed src-old

--- src-old/getDirectories.js
+++
@@ -1,6 +0,0 @@
-var fs = require('fs');
-var path = require('path');
-
-module.exports = function getDirectories(absPath) {
-  return fs.readdirSync(absPath).filter((file) => fs.statSync(path.join(absPath, file)).isDirectory());
-}
