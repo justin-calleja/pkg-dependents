e3843d095c75ab8761a933b5210d406d371b41b7
Justin Calleja <calleja.justin@gmail.com>
2016-06-19 16:05:01 +0200

    Added documentation to main function

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,20 @@ export interface Opts {
   recurse: boolean;
 }
 
-export default function pkgDependents(pkgName: string, opts: Opts, cb: (err: Error, result: IndexInfoDict) => void) {
+/**
+ * Given an array of directory paths housing Node packages (projects), each package's package.json is read
+ * and a data-structure (IndexInfoDict) is returned with an entry for each package and information
+ * about each package and its dependents (packages which depend on it).
+ *
+ * @param  {string}        pkgName The package in which you're interested in (limits scope of contents in IndexInfoDict)
+ * @param  {Opts}          opts    Contain paths: string[] of paths in which to look for Node packages. Also contains 'recurse'
+ *                                 option which will, apart from data about pkgName, will also include data about pkgName's
+ *                                 dependents (and their dependents etc…) in the resulting IndexInfoDict (as long as these
+ *                                 dependent packages are also in given opts.paths)
+ * @param  {IndexInfoDict} cb      The callback which will be given the result or error.
+ * @return {void}                  No return value (use cb)
+ */
+export default function pkgDependents(pkgName: string, opts: Opts, cb: (err: Error, result: IndexInfoDict) => void): void {
   var paths = opts.paths || [];
   var recurse = opts.recurse || false;
 
